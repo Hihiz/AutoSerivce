73262c465025da532b9a4e4977c1dbcec2a43118
Hihiz <public@mail.ru>
2023-01-30 22:19:51 +0300

    Сделать внедрение зависимостей

--- AutoSerivce/App.xaml.cs
+++ AutoSerivce/App.xaml.cs
@@ -1,9 +1,61 @@
-﻿using System.Windows;
+﻿using AutoSerivce.Interfaces;
+using AutoSerivce.Interfaces.Implementations;
+using AutoSerivce.ViewModels;
+using AutoSerivce.Views.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Windows;
 
 namespace AutoSerivce
-{  
+{
     public partial class App : Application
     {
+        private static IServiceProvider? _services;
 
+        public static IServiceProvider Services => _services ??= InitializeServices().BuildServiceProvider();
+
+        private static IServiceCollection InitializeServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton<MainWindowViewModel>();
+            services.AddScoped<AddEditServiceWindowViewModel>();
+
+            services.AddSingleton<IUserDialog, UserDialogService>();
+
+            services.AddSingleton(
+              s =>
+              {
+                  var model = s.GetRequiredService<MainWindowViewModel>();
+                  var window = new MainWindow { DataContext = model };
+
+                  model.DialogComplete += (_, _) => window.Close(); //
+
+
+                  return window;
+              });
+
+            services.AddTransient(
+               s =>
+               {
+                   var scope = s.CreateScope();
+                   var model = scope.ServiceProvider.GetRequiredService<AddEditServiceWindowViewModel>();
+                   var window = new AddEditServiceWindow { DataContext = model };
+
+                   model.DialogComplete += (_, _) => window.Close();// 
+                   window.Closed += (_, _) => scope.Dispose(); // 
+
+                   return window;
+               });
+
+            return services;
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Services.GetRequiredService<IUserDialog>().OpenMainWindow();
+        }
     }
 }
