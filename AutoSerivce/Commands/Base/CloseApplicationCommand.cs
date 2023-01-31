68cb4d7e2cc7149306c1dadadbb3f23b7c002fc2
Hihiz <public@mail.ru>
2023-01-31 16:02:52 +0300

    Сделать команды закрития приложения и окна

---
+++ AutoSerivce/Commands/Base/CloseApplicationCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+
+namespace AutoSerivce.Commands.Base
+{
+    public class CloseApplicationCommand : Command
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public override bool CanExecute(object? parameter) => true;
+
+        public override void Execute(object? parameter) => Application.Current.Shutdown();
+
+    }
+}
---
+++ AutoSerivce/Commands/Base/CloseWindowCommand.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace AutoSerivce.Commands.Base
+{
+    public class CloseWindowCommand : Command
+    {
+        public override bool CanExecute(object? parameter) => parameter is Window;
+
+        public override void Execute(object? parameter)
+        {
+            if (!CanExecute(parameter)) return;
+
+            var window = (Window)parameter;
+            window.Close();
+        }
+    }
+
+    class CloseDialogCommand : Command
+    {
+        public bool? DialogResult { get; set; }
+
+        public override bool CanExecute(object? parameter) => parameter is Window;
+
+        public override void Execute(object? parameter)
+        {
+            if (!CanExecute(parameter)) return;
+
+            var window = (Window)parameter;
+            window.DialogResult = DialogResult;
+            window.Close();
+        }
+    }
+}
