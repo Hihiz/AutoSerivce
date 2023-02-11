2c3d2fc615eacedf68628abcee801840f3f84d52
Hihiz <public@mail.ru>
2023-02-12 02:43:06 +0300

    Модель представления клиентов

---
+++ AutoSerivce/ViewModels/ClientServiceWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using AutoSerivce.Models;
+using AutoSerivce.ViewModels.Base;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AutoSerivce.ViewModels
+{
+    public class ClientServiceWindowViewModel : DialogViewModel
+    {
+        public ClientServiceWindowViewModel()
+        {
+            using (AutoServiceContext db = new AutoServiceContext())
+            {
+                ClientSerivces = db.ClientServices.ToList();
+            }
+        }
+
+        #region Свойства
+
+        private IEnumerable<ClientService> _clientSerivces;
+        public IEnumerable<ClientService> ClientSerivces { get => _clientSerivces; set => Set(ref _clientSerivces, value); }
+
+        #endregion
+    }
+}
