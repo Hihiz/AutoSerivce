14502b1ba8bb419a6a1a3416b937a42b288d9440
Hihiz <public@mail.ru>
2023-02-11 19:24:58 +0300

    Команда закрытия приложения

--- AutoSerivce/Commands/Base/CloseApplicationCommand.cs
+++ AutoSerivce/Commands/Base/CloseApplicationCommand.cs
@@ -10,6 +10,5 @@ namespace AutoSerivce.Commands.Base
         public override bool CanExecute(object? parameter) => true;
 
         public override void Execute(object? parameter) => Application.Current.Shutdown();
-
     }
 }
